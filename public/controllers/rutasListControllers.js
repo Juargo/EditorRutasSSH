354cfe747944ab86a3e8b0a10699954e6bc06940
juargo <jorgeretamalaburto@gmail.com>
2016-11-16 17:44:48 -0300

    nueva forma

--- public/controllers/rutasListControllers.js
+++ public/controllers/rutasListControllers.js
@@ -1,14 +1,45 @@
 angular.module("routerApp")
-.controller("rutasListController",function($scope){
-    var selectedProtocol = null;
+    .constant("operadoraActiveClass","btn-primary")
+    .controller("rutasListController", function ($scope,operadoraActiveClass) {
+        var selectedOperadora = null;
+        var selectedNodo = null;
+        var selectedProtocol = null;
 
-    $scope.selectProtocol = function(event,clase){
-        $scope.selectedProtocol = $(event.target).html();
-        $('.'+ clase).removeClass("active");
-        $(event.target).parent().addClass("active");
-    }
+        $scope.selectOperadora = function (newOperadora) {
+            selectedOperadora = newOperadora;
+        }
+        $scope.getOperadoraClass = function (operadora) {
+            return selectedOperadora == operadora ? operadoraActiveClass : "";
+        }
 
-    $scope.getactiveProtocol = function(nodoclass){
-        return $("."+nodoclass).hasClass("active")
-    }
-})
+        $scope.operadoraFilterFn = function (ruta) {
+            return selectedOperadora == null || ruta.operadora == selectedOperadora;
+        }
+
+        $scope.selectNodo = function(newNodo){
+            selectedNodo = newNodo;
+        }
+
+        $scope.getNodoClass = function(Nodo){
+            return selectedNodo == Nodo ? operadoraActiveClass : ""
+        }
+
+        $scope.nodoFilterFn = function(Nodo){
+            var valor=false;
+            if(angular.isObject(selectedNodo)){
+                valor = Nodo.nombre == selectedNodo.nombre;
+            }
+            return valor;
+        }
+
+
+        $scope.selectProtocol = function (event, clase) {
+            $scope.selectedProtocol = $(event.target).html();
+            $('.' + clase).removeClass("active");
+            $(event.target).parent().addClass("active");
+        }
+
+        $scope.getactiveProtocol = function (nodoclass) {
+            return $("." + nodoclass).hasClass("active")
+        }
+    })
