8841397a0336366983c69d7f1f8ccd411fd14a45
juargo <jorgeretamalaburto@gmail.com>
2016-11-23 15:26:58 -0300

    mejora jquery angular

--- public/controllers/addControllers.js
+++ public/controllers/addControllers.js
@@ -12,17 +12,21 @@ angular.module("addrouterApp")
     $scope.add_ruta_button = function(event){
         var ruta    = '<tr class="tr_host_'+index_ruta+'">\
                             <td rowspan="1" class="rowspan_ruta">\
-                                <small><strong> Ruta</strong> #'+(index_ruta+1)+'</small>\
-                                <button class="delete_ruta_button btn btn-danger btn-xs" type="button"  data-ruta='+index_ruta+' >\
+                                <samp>Ruta: '+(index_ruta+1)+'</samp>\
+                                <button class="btn btn-danger btn-xs" type="button"  data-ruta='+index_ruta+' ng-click="delete_ruta_button($event)" >\
                                     <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>\
                                 </button>\
                             </td>\
-                            <td><input class="form-control" ng-model="nodo.protocolos.ssh[0][0].port"></td>\
+                            <td><input class="form-control" ng-model="nodo.protocolos.ssh['+index_ruta+'][0].port"></td>\
                             <td><input class="add_salto_button" type="button" value=">" data-ruta="1" data-salto="0"></td>\
                         </tr>';
 
         $("tbody").append($compile(ruta)($scope));
 
         index_ruta ++;
+    }
+
+    $scope.delete_ruta_button = function(event){
+        $(event.target).parent().parent().remove();
     }
 })
