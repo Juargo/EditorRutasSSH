fb7b0a0d6245af086f35fb101e56b76bf84f801a
juargo <jorgeretamalaburto@gmail.com>
2016-11-23 12:46:52 -0300

    mejora add

--- public/controllers/addControllers.js
+++ public/controllers/addControllers.js
@@ -3,19 +3,26 @@ angular.module("addrouterApp")
     $scope.nodo={};
     $scope.nodo.protocolos = {};
     $scope.nodo.protocolos.ssh = [];
+    index_ruta=0;
 
     $scope.selecting = function (){
      console.log($scope.nodo)   
     }
 
     $scope.add_ruta_button = function(event){
-        var index_ruta  =  $(event.target).data('ruta');
-        var ruta    = '<tr class="tr_host_1">\
-                            <td rowspan="1" class="rowspan_ruta"><small><strong> Ruta</strong> #1</small>\
+        var ruta    = '<tr class="tr_host_'+index_ruta+'">\
+                            <td rowspan="1" class="rowspan_ruta">\
+                                <small><strong> Ruta</strong> #'+(index_ruta+1)+'</small>\
+                                <button class="delete_ruta_button btn btn-danger btn-xs" type="button"  data-ruta='+index_ruta+' >\
+                                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>\
+                                </button>\
+                            </td>\
                             <td><input class="form-control" ng-model="nodo.protocolos.ssh[0][0].port"></td>\
                             <td><input class="add_salto_button" type="button" value=">" data-ruta="1" data-salto="0"></td>\
                         </tr>';
 
         $("tbody").append($compile(ruta)($scope));
+
+        index_ruta ++;
     }
 })
