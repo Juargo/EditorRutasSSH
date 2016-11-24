b4160785bcafbf2b12952992d9d6797897ecdb39
juargo <jorgeretamalaburto@gmail.com>
2016-11-24 18:23:48 -0300

    salto

--- public/controllers/addControllers.js
+++ public/controllers/addControllers.js
@@ -18,7 +18,7 @@ angular.module("addrouterApp")
                                 </button>\
                             </td>\
                             <td><input class="form-control" ng-model="nodo.protocolos.ssh['+ index_ruta + '][0].port"></td>\
-                            <td><input class="add_salto_button" type="button" value=">" data-ruta="1" data-salto="0"></td>\
+                            <td><button class="add_ruta_button btn btn-default btn-xs" type="button" ng-click="add_salto_button($event)" data-salto="0">></button></td>\
                         </tr>';
 
             $("tbody").append($compile(ruta)($scope));
@@ -27,19 +27,41 @@ angular.module("addrouterApp")
         }
 
         $scope.delete_ruta_button = function (event) {
-           // if ($(event.target)[0].nodeName == "SPAN") {
-           //     var res = $(event.target).parent().parent().parent()[0].innerText;
-           //     var split = res.split(" ");
-           //     $(event.target).parent().parent().parent().remove();
-           // } else {
-           //     var res = $(event.target).parent().parent()[0].innerText;
-           //     var split = res.split(" ");
-           //     $(event.target).parent().parent().remove();
-           // }
-           //delete $scope.nodo.protocolos.ssh[split[1]-1];
+           var array = [] = $scope.nodo.protocolos.ssh;
+           if ($(event.target)[0].nodeName == "SPAN") {
+                var res = $(event.target).parent().parent().parent()[0].innerText;
+                $(event.target).parent().parent().parent().remove();
+            } else {
+                var res = $(event.target).parent().parent()[0].innerText;
+                $(event.target).parent().parent().remove();
+           }
+           var split = res.split(" ");
+           var index_delete = split[1]-1;
+
+           delete array[index_delete];
+           //console.log(index_delete);
+           //array.splice(index_delete,1);
            //$scope.nodo.protocolos.ssh.sort(function(a,b){
            //    return a-b;
            //});
            //console.log($scope.nodo.protocolos.ssh);
+        }
+
+        $scope.add_salto_button = function(event){
+            var res = $(event.target).parent().parent()[0].innerText;
+            var split = res.split(" ");
+            var index_ruta = split[1]-1;
+            var index_salto = $(event.target).data("salto");
+
+            var salto      =   '<tr>\
+					        	    <td><input class="form-control" ng-model="nodo.protocolos.ssh['+ index_ruta + ']['+index_salto+'].port"></td>\
+					        	</tr>';
+
+            $(event.target).parent().parent().after(salto);  
+            
+            rowantes = $(event.target).parent().parent()[0].firstElementChild.attributes[0].value;
+           $(event.target).parent().parent()[0].firstElementChild.attributes[0].value = parseInt(rowantes,10) + 1;
+
+           // $(event.target).data("salto",parseInt(index_salto,10)+1)
         }
     })
